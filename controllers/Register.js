f1179a440cb28ac19440bac89cc03fa2ff919786
FalachYusuf_ <falachachsan90@gmail.com>
2023-05-17 20:18:15 +0900

    Add validation in register module

--- controllers/Register.js
+++ controllers/Register.js
@@ -8,6 +8,24 @@ export const Register = async (req, res) => {
   const salt = await bycrypt.genSalt();
   const hashPassword = await bycrypt.hash(password, salt);
   try {
+    // Validation duplicate username
+    const existingnameUser = await Users.findOne({
+      where: {
+        name: name,
+      },
+    });
+    if (existingnameUser) {
+      return res.status(400).json({ msg: 'Username sudah terdaftar' });
+    }
+    // Validation duplicate email
+    const existingemailUser = await Users.findOne({
+      where: {
+        email: email,
+      },
+    });
+    if (existingemailUser) {
+      return res.status(400).json({ msg: 'Email sudah terdaftar' });
+    }
     await Users.create({
       name: name,
       email: email,
@@ -15,6 +33,6 @@ export const Register = async (req, res) => {
     });
     res.json({ msg: 'Registrasi berhasil' });
   } catch (error) {
-    console.log(error);
+    res.status(500).json({ msg: 'Terjadi kesalahan saat melakukan registrasi' });
   }
 };
