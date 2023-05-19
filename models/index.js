5bc5a577a8b5619e8ddc1806995d4fd2ecb50c0a
FalachYusuf_ <falachachsan90@gmail.com>
2023-05-19 22:51:02 +0900

    Centralized income models

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
 import Users from './allModels/UserModels.js';
+import Incomes from './allModels/IncomeModels.js';
 const model = {};
 
 model.Users = Users;
+model.Incomes = Incomes;
 export default model;
