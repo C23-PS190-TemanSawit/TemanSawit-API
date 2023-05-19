d2a33f53b8dd7a575204b45e6b730bf42bfcb506
FalachYusuf_ <falachachsan90@gmail.com>
2023-05-19 23:35:26 +0900

    Adding relation function and adding command to create table 'await db.sync'

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import router from './routes/routes.js';
 import cookieParser from 'cookie-parser';
-// ### import DB models
+import Relations from './models/RelationModels.js';
 
 dotenv.config();
 const app = express();
@@ -14,10 +14,12 @@ try {
   await db.authenticate();
   console.log('db connected');
   // ### Command to create db
+  // await db.sync({ force: true });
 } catch (error) {
   console.log(error);
 }
-
+// Function to call relationships table
+Relations();
 app.use(cors({ credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
