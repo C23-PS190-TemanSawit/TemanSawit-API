15c01bd07918a7302795070774cc932d8aa19164
Rschnrlhdyt <rischanurul.hidayati@gmail.com>
2023-05-20 10:14:15 +0800

    fix type on conntroller to controller

--- controllers/allControllers/IncomeControllers.js
+++ controllers/allControllers/IncomeControllers.js
@@ -2,15 +2,15 @@ import model from "../../models/index.js";
 const controller = {};
 
 //Create a new incomes transaction
-conntroller.postIncome = async (req, res) => {
+controller.postIncome = async (req, res) => {
     const { userId, transaction_time, price, total_weight, description } = req.body;
     try {
-        await model.Incomes.create({
-            userId: userId,
-            transaction_time: transaction_time,
-            price: price,
-            total_weight: total_weight,
-            description: description,
+      await model.Incomes.create({
+        userId: userId,
+        transaction_time: transaction_time,
+        price: price,
+        total_weight: total_weight,
+        description: description,
     });
     res.status(200).json({ msg: 'Berhasil menambah transaksi' });
   } catch (error) {
