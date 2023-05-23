f2d3c51b83d92b2d180da503fdd256058210f9d2
rschnrlhdyt <112224976+Rschnrlhdyt@users.noreply.github.com>
2023-05-23 10:31:41 +0800

    update user password
    tolong ditesting

--- controllers/allControllers/UsersControllers.js
+++ controllers/allControllers/UsersControllers.js
@@ -15,5 +15,28 @@ controller.getUsers = async (req, res) => {
 };
 
 // Update users password
+controller.updatePassword = async (req, res) => {
+  try {
+    const { userId, currentPassword, newPassword } = req.body;
+
+    const user = await model.Users.findByPk(userId);
+    if (!user) return res.status(404).json({ msg: 'Username tidak ditemukan' });
+
+    const isPasswordValid = await bcrypt.compare(currentPassword, user.password);
+    if (!isPasswordValid)
+      return res.status(401).json({ msg: 'Password saat ini tidak valid, silakan coba lagi' });
+
+    const salt = await bcrypt.genSalt();
+    const hashPassword = await bcrypt.hash(newPassword, salt);
+
+    user.password = hashPassword;
+    await user.save();
+
+    res.json({ msg: 'Password berhasil diperbarui' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: 'Terjadi kesalahan saat memperbarui password' });
+  }
+};
 
 export default controller;
