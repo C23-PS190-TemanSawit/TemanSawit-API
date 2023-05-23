de9287c51bc60abb1539889d669a3f9d39b2b8d8
FalachYusuf_ <falachachsan90@gmail.com>
2023-05-23 11:53:10 +0900

    adding outcome routes

--- routes/routes.js
+++ routes/routes.js
@@ -5,17 +5,22 @@ import verifyToken from '../middleware/verifyToken.js';
 const route = '/api';
 
 const router = express.Router();
-// Login routes
+// Auth routes
 router.get(`${route}/users`, verifyToken, controller.user.getUsers);
 router.get(`${route}/token`, controller.token.refreshToken);
 router.post(`${route}/users`, controller.reg.Register);
 router.post(`${route}/login`, controller.access.Login);
+router.put(`${route}/update`, verifyToken, controller.user.updatePassword);
 router.delete(`${route}/logout`, controller.access.Logout);
-
 // Income routes
 router.post(`${route}/income`, verifyToken, controller.income.postIncome);
 router.get(`${route}/income`, verifyToken, controller.income.getUserIncome);
 router.get(`${route}/income/sort`, verifyToken, controller.income.sortIncomeByTime);
 router.get(`${route}/income/:incomeId`, verifyToken, controller.income.getIncomeByID);
+// Outcome routes
+router.post(`${route}/outcome`, verifyToken, controller.outcome.postOutcome);
+router.get(`${route}/outcome`, verifyToken, controller.outcome.getUserOutcome);
+router.get(`${route}/outcome/sort`, verifyToken, controller.outcome.sortOutcomeByTime);
+router.get(`${route}/outcome/:outcomeId`, verifyToken, controller.outcome.getOutcomeByID);
 
 export default router;
