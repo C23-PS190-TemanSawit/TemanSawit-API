4b59cdc660ef6f54207bfe4810cd1901a08fa404
FalachYusuf_ <falachachsan90@gmail.com>
2023-05-23 11:59:03 +0900

    Committing local changes before merge

--- controllers/allControllers/UsersControllers.js
+++ controllers/allControllers/UsersControllers.js
@@ -1,4 +1,5 @@
 import model from '../../models/index.js';
+import bcrypt from 'bcrypt';
 const controller = {};
 
 // Get Users
@@ -15,5 +16,36 @@ controller.getUsers = async (req, res) => {
 };
 
 // Update users password
+controller.updatePassword = async (req, res) => {
+  const userId = req.userId;
+  let { password, newPassword, confPassword } = req.body;
+  try {
+    const user = await model.Users.findOne({
+      id: userId,
+    });
+    // Cek current password is match
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) return res.status(400).json({ msg: 'Password yang anda masukkan salah' });
+
+    // Cek updated password match with confirm password
+    if (newPassword !== confPassword) return res.status(400).json({ msg: 'Password tidak cocok, silahkan masukkan kembali password anda' });
+    const salt = await bcrypt.genSalt();
+    const hashPassword = await bcrypt.hash(newPassword, salt);
+
+    await model.Users.update(
+      { password: hashPassword },
+      {
+        where: {
+          userId: userId,
+        },
+      }
+    );
+    await user.save();
+    return res.status(200).json({ msg: 'Update password berhasil' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: 'Terjadi kesalahan saat update password' });
+  }
+};
 
 export default controller;
