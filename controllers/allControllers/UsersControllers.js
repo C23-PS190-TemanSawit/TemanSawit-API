66bba3373044793f34e7ae47f5f3e4d3b78c4597
FalachYusuf_ <falachachsan90@gmail.com>
2023-06-02 21:56:12 +0900

    Adding get User Profile function and forgot password

--- controllers/allControllers/UsersControllers.js
+++ controllers/allControllers/UsersControllers.js
@@ -54,21 +54,15 @@ controller.updateProfile = async (req, res) => {
   const { fullName, phoneNumber, birthDate, gender } = req.body;
   try {
     const user = await model.Users.findOne({
-      where: { userId: userId },
+      userId: userId,
     });
-
-    // Check if the user is authorized to update their profile
-    if (user.userId !== userId) {
-      return res.status(401).json({ msg: 'Unauthorized' });
-    }
-
-    // Check if one of the fields is empty
-    if (fullName === undefined || phoneNumber === undefined || birthDate === undefined || gender === undefined) {
-      return res.status(400).json({ msg: 'Harap lengkapi semua data' });
-    }
-
     await model.Users.update(
-      { fullName, phoneNumber, birthDate, gender },
+      {
+        fullName: fullName,
+        phoneNumber: phoneNumber,
+        birthDate: birthDate,
+        gender: gender,
+      },
       {
         where: {
           userId: userId,
@@ -84,3 +78,54 @@ controller.updateProfile = async (req, res) => {
 };
 
 export default controller;
+
+controller.forgotPassword = async (req, res) => {
+  const { username, newPassword, confPassword } = req.body;
+  const user = await model.Users.findOne({
+    where: {
+      username: username,
+    },
+  });
+  // Check if username exists
+  if (!username) return res.status(401).json({ message: 'Username tidak ditemukan' });
+  try {
+    // Cek updated password match with confirm password
+    if (newPassword !== confPassword) return res.status(400).json({ msg: 'Password tidak cocok, silahkan masukkan kembali password anda' });
+    const salt = await bcrypt.genSalt();
+    const hashPassword = await bcrypt.hash(newPassword, salt);
+    await model.Users.update(
+      { password: hashPassword },
+      {
+        where: {
+          username: username,
+        },
+      }
+    );
+    await user.save();
+    return res.status(200).json({ msg: 'Update password berhasil' });
+  } catch (error) {
+    res.status(500).json({ msg: 'Terjadi kesalahan saat update password' });
+    console.log(error);
+  }
+};
+
+// Get user profile from databases
+controller.getUserProfile = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const profileImage = req.body.image;
+    const img = await model.Users.findOne({
+      image: profileImage,
+      attributes: ['userId', 'username', 'fullName', 'email', 'image', 'phoneNumber', 'birthDate', 'gender'],
+      where: {
+        userId: userId,
+      },
+    });
+    res.status(200).json(img);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'File tidak ditemukan',
+    });
+  }
+};
