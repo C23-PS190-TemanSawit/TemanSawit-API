ed139f0a2ef7d1983f1015614a10dfe5584dee05
FalachYusuf_ <falachachsan90@gmail.com>
2023-06-03 20:04:39 +0900

    Adding revision

--- config/gcs.js
+++ config/gcs.js
@@ -3,10 +3,10 @@ dotenv.config();
 import { Storage } from '@google-cloud/storage';
 import { writeFileSync } from 'node:fs';
 
-writeFileSync('gcs-test.json', process.env.GCS_KEY);
+writeFileSync('./gcs-test.json', process.env.GCS_KEY);
 
 const storage = new Storage({
-  keyFilename: 'gcs-test.json',
+  keyFilename: './gcs-test.json',
   projectId: process.env.GCS_PROJECTID,
 });
 
